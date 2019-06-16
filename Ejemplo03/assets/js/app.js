354bf6ee37d1cff9852115e22eb01a7e6ba92566
Eduardo <eduardo.linares@outlook.com>
2019-06-16 01:44:37 -0400

    ADD Ejemplo03

---
+++ Ejemplo03/assets/js/app.js
@@ -0,0 +1,34 @@
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
+
+  
+  $("#validationCustom01").val();
+
+  $( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+function fn_login() {
+    document.getElementById("txtsaludo").value = "Paragraph changed.";
+ 
+}
+
+
+  
